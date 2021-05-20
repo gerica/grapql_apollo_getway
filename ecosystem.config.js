8b37aa598894fd42996667555645852bf4cf34c7
gerica <rogeriogerica@gmail.com>
2021-05-20 14:00:37 -0300

    pm2 start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'RadiolifeGateway',
-      script: './src/server.js',
+      script: './startInEc2.sh',
     },
   ],
   deploy: {
@@ -12,7 +12,7 @@ module.exports = {
       key: '~/Projetos/radiolife/Radiolife-us-east-2-dp.pem',
       ref: 'origin/main',
       repo: 'git@github.com:gerica/grapql_apollo_getway.git',
-      path: '/home/ubuntu/projects/graphql',
+      path: '/home/ubuntu/projects/',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
     },
   },
