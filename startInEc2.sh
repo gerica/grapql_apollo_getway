3875487b2278877d579952cd4e0d142aa5b56bf5
gerica <rogeriogerica@gmail.com>
2021-05-19 21:27:51 -0300

    nome do servico

--- startInEc2.sh
+++ startInEc2.sh
@@ -1,2 +1,2 @@
-pm2 start src/server.js
-pm2 start src/serverLocal.js
+pm2 start src/server.js --name "RadiolifeGateway"
+pm2 start src/serverLocal.js --name "RadiolifeGatewayLocal"
--- stopInEc2.sh
+++ stopInEc2.sh
@@ -1,2 +1,2 @@
-pm2 stop server
-pm2 stop serverLocal
+pm2 stop RadiolifeGateway
+pm2 stop RadiolifeGatewayLocal
