5abaa402c21fbba820fc24639bae090ebf88671d
gerica <rogeriogerica@gmail.com>
2021-05-22 10:29:26 -0300

    deploy local

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 echo "Kill all the running PM2 actions"
 sudo pm2 kill
+
+echo "Jump to app folder"
+cd /projects/graphql/current
+
+echo "Run new PM2 action"
+sudo pm2 start /projects/graphql/current/ecosystem.config.json
